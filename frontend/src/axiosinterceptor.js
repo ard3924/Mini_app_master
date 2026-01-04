e1d9e1066ddf32697a42508ace3af7a8c6ea9c4e
ard3924 <aravindr3924@gmail.com>
2026-01-04 10:12:02 +0530

    changed base URL for frontend with deployed backend URL

--- frontend/src/axiosinterceptor.js
+++ frontend/src/axiosinterceptor.js
@@ -1,8 +1,9 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:  "http://localhost:5000/api",
+    // baseURL:  "http://localhost:5000/api",
     // baseURL:  "/api",
+    baseURL:  "https://sow-backend-7po9.onrender.com/api",
 
 });
 
